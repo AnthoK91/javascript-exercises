7f8210627ae1b7284294b5b627fd4aae94173ca9
Anthony Kirkham <antho.kirkham@gmail.com>
2023-11-23 20:01:41 +1000

    Second test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    wordHold = []
+    if(number < 0) {
+        wordHold.push("ERROR")
+    } else {
+    for (let i = 0; i < number; i++) {
+			wordHold.push(word);
+    }
+}
+    return wordHold.join("")
 
 };
 
